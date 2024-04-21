8c1d1495a14781a1d6e534a16550a6c5a48c0744
danielJL19 <danieljmnz15@hotmail.com>
2024-04-21 16:21:31 -0400

    after_create payment omited

--- app/controllers/dashboard/admin/users_controller.rb
+++ app/controllers/dashboard/admin/users_controller.rb
@@ -2,4 +2,12 @@ class Dashboard::Admin::UsersController < DashboardController
   def index
     @users = User.all.decorate
   end
+
+  # after_create :add_membership_payment
+
+  # def add_membership_payment
+  #   10.times do |i|
+  #     MembershipPayment.create()
+  #   end
+  # end
 end
