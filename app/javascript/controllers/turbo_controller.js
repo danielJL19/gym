96b7170778109d983964713970748c00c99ee5d3
danielJL19 <danieljmnz15@hotmail.com>
2024-04-23 13:40:59 -0400

    implements new form to register users from admin

---
+++ app/javascript/controllers/turbo_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="turbo"
+export default class extends Controller {
+  connect() {
+    this.element.setAttribute('data-action', "click->turbo#click")
+  }
+
+  click(e){
+    e.preventDefault();
+    //obtenemos la url de ese link
+    var url = this.element.getAttribute('href')
+    //hacemos una peticion via fetch y que nos responda mediante un turbo_stream
+    fetch(this.url,{
+      headers: {
+        Accept: "text/vnd.turbo-stream.html"
+      }
+    })
+    .then(r=> r.text())
+    .then(html => Turbo.renderStreamMessage(html) )
+  }
+}
